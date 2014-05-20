e9a06bd2f29030381de1683f63cbdf8d44d2d50b
caicaiking <caicaiking@gmail.com>
2014-05-20 23:56:44 +0800

    屏蔽了plot.cpp的Debug信息。

--- AnalysisMode/Plot.cpp
+++ AnalysisMode/Plot.cpp
@@ -705,7 +705,7 @@ void Plot::turnOffRefTrace()
 
     for(int i=0; i< keys.length(); i++)
     {
-        qDebug()<<"curves "<<keys.at(i);
+        //qDebug()<<"curves "<<keys.at(i);
         if(keys.at(i)!=0)
         {
             curves.value(keys.at(i)).cur1->detach();
