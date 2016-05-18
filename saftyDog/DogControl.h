12882f24d033ff2c3eaadf9d7b691fecaf42687e
Abama <caicaiking@gmail.com>
2016-05-18 17:27:37 +0800

    修改了加密狗

--- saftyDog/DogControl.h
+++ saftyDog/DogControl.h
@@ -15,23 +15,20 @@
 
 const unsigned char vendor_code[] =
 
-        "yBRjJgT5UOYzMt4InDV6OVbALoodYYSxMeI2ATgTWJafG0zdqr482PG3+"
-        "VWslgAQOQuLLKJjjUyTfZGpI3sFE1qIFTAtzdnLknEfUC9Xpu1UtzNbuRT"
-        "DqXDQsh8qRz4CIWNnSl5e26M1tUQlh9mcKaW8YqiXfxWe7FcUCExh5ELxTF"
-        "6K1EczECwgbSTTCw4U3vY8QWlzwPf6bOG3HauTtJmirsZisoVXr7E0yP6m7"
-        "xr9+ZKx2pbA4ZP+PBP88zp229vl6/Ykm9uWyU7V7vGvGTozwiBbJKzB3kY9"
-        "Ca5AKMlsJX3W+LAqL4jOly+oKfLi+2IyfPm+q/ZE1jmczdNsKUhDFj/tnx4s"
-        "izpgeXmEGAzBZbwxFAtj8u0H5e9Xt8W9vinGjPQyQBi8PXjw1GY9/xGNsb/7"
-        "PnpRPwd0qZqyGMzJr55QyXiHRrXQ+rPQr4zWs83Ng6Eyq58LMzxu2llsNqLwy"
-        "11uBj01/7Kx3yehjnjO/Eutql+v9XhB2Ycw/HUqBUEXtUiCqnmTinIFtfbVjWg"
-        "jX6MjrJNOUFT6YVC2c2oAGLfnf9RKWGUUW97k0nCdWdIDUWbum2dgM5FUNI7y9"
-        "22DyWqF6mYBWyROnjtBnhFtDR1Jqpop/JvjTiANl4bisDgnPNfvr8Lh2OiYhXgi"
-        "JXpab1sZisGY7x9XLC6GFsN8o257Sk9sK9tabY/ZkUTf13veiwZp1eBKgfHs+KU"
-        "fdL/+HCU/GkOHw6Rl/3grdQP4LlKsEFOLcH0+wD+ZOZUMVdmzB7zghCL2p4wNZV"
-        "oRtucLwFsVnACitcazVVDRSm3+zaKI9au1LY1rtCn+tqIrkcpmXuJDTRRz5j8pg"
-        "KDEuw9SuXdH3QEcFWLjWuV9GYgL8ztk717sgFAXcEDB4fRuDJwa1Hfyf542Lbgr"
-        "8SE00ZU9UWiXrFEh8pL9WPzEsh/EUWi27h7j72AoIvJUjVFRFoCSJ+/Y5WcYU/T"
-        "WqrfaXA==";
+        "FYqLT8uT6HadrrztaO1WVyZ3IOEtBqqIX4unLZFEMmUnyuu9WfcAQMIxpNG60vfBXDpLCUn2Aq"
+        "gro8oB/Ds0Q8DNzrtF0XUhXzPiMdwuCaNL20udvhRFMaR63LeAFX4tCX8V+WHURb0MqMIIRdcX"
+        "Mhxv3D/MUZ2wiUVjxe71Wgr9/16U46gU5XZlLM2XAUDySgAYKzF0JQ7X6duj2uh78TVgCR6+J1"
+        "ZWwEJvpPFhbQDBdTpUkpnyfX+ASBmtCHan66LbiS+OyYvgG8jdOrHXe/OgWuR3gAS9rGCFM/0n"
+        "by6D9Ops4rBls5kxuWbLIi+oFUpq1tSaGOdoRpfLho+derZ4ouKYMahF7J4KbfjCm8OTTNJZc+"
+        "N8C4j9gGZjC4ZKTubTKQYTYxe5n6DBwlxw5jsLhMpCOGpAaZ4wywOCTOD0JhGD+dImDgGB9/ex"
+        "CMgqw3aJhW9YJ4GSD9W4UPVllTL66+vB8p5CnocP5TPopUVtzhN9gjHhxh8R+i7UzIRJuIyPNB"
+        "WzBNr/CbUTL+AJ54HwGPnyLNirHNxdvSysCSoh+UpE8SBgIQWowZu0uW2YWvh+Xalb+W596yNJ"
+        "3FDAS8hiIVSrP1fTRzq3c0HVoGweoKWWzuLb8SdFDBahX9jD3/H8CvUGCwqfiQhpoeJ3JWJig0"
+        "ZmCcC3rC8mUfutaDVoFq6Ww2R0onoiwYQOAo6bN9qk0+WO96QrnY51Hd6KBY5JAsF7YRGYe9ft"
+        "77muSAgwkYeKnBFSeuKMJL9DI8SOItZIwqoXFdp2Y8WgKh6DgdzlqyiDfo3W5f+jNDRQyKTWVa"
+        "cM9nWHXXVhWa5Fd+cjSu4Le+MXmpiTtfEoljNPukvMcSCgqpMf8NfW3bzrxC68njRdoaNeUzG6"
+        "ufeiv7MJp12QVhXXAPzEZ5WmB12bj5A0aBAVQe6Q04HgR0Y/Q9XL+N8aJpJmME6AXHoqUcU7iu"
+        "3K7p8iQyyN5wrV6FnXFw==";
 
 //WKE FactoryView 2014
 #define ENCRYPT_BUFFER_FEATUREID1 1    //feature id which is selected
@@ -42,7 +39,7 @@ const int isString1 = 1;    //This is a string buffer
 /*The encrypted array is in UTF-8 format. Please convert it to proper format before using it.*/
 
 const unsigned char encryptStrArr1[20] = {
-    0x81, 0x2B, 0x76, 0xAC, 0x86, 0x1A, 0xF4, 0x35, 0xD2, 0x11, 0x5C, 0x5B, 0x49, 0x5D, 0xEF, 0xE0, 0x08, 0x91, 0xD4, 0x3C
+   0x38, 0x20, 0xC1, 0x28, 0x8B, 0xD9, 0x20, 0x8C, 0xF2, 0x55, 0x5B, 0x15, 0x28, 0xBC, 0x71, 0x52, 0x9C, 0xFC, 0xED, 0x79
  };
 
 typedef __int32 ENCRYPT_DATA_TYPE1;
